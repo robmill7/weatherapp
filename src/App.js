53aa2d6d097d627359aca116f412261c48063b79
robmill7 <rmiller2081@yahoo.com>
2023-06-01 23:25:52 -0400

    formatted weather data for display

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ import TimAndLocation from './components/TimAndLocation';
 import TemperaturAndDetails from './components/TemperaturAndDetails';
 import Forcast from './components/Forcast';
 import getFormattedWeatherData from './services/weatherService';
+import {useEffect, useState } from 'react';
 
 
 
 
 
 function App() {
+
+  const [query,setQuery] = useState ({q: 'berlin'})
+  const [units, setUnits] = useState('metric')
+  const [weather, setWeather] = useState(null)
+
+  useEffect(()=> {
+    const fetchWeather = async () => {
+       await getFormattedWeatherData({...query, units}).then((data) =>{
+        setWeather(data);
+       });
+    };
+    
+    fetchWeather();
+  }, [query, units])
+
+
 const fetchWeather = async () => {
   const data = await getFormattedWeatherData( {q: 'london'});
   console.log(data);
@@ -24,11 +41,15 @@ return(
    from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
     <TopButtons />
     <Inputs />
-
-    <TimAndLocation />
+    {weather && (
+      <div>
+        <TimAndLocation weather = {weather}/>
     <TemperaturAndDetails />
     <Forcast title='hourly forcast'/>
     <Forcast title='daily forcast'/>
+      </div>
+    )}
+    
 
   </div>
 );
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,7 +1,7 @@
 import { DateTime } from "luxon";
 
 const apiCall = process.env.REACT_APP_apiCall;
-const BASE_URL = process.env.REACT_APP_BASE_URL; // url not found need fix
+const BASE_URL = process.env.REACT_APP_BASE_URL; 
 
 const getWeatherData = (infoType, searchParams) => {
     const url = new URL(BASE_URL + '/' + infoType); // will replace weather
@@ -30,28 +30,29 @@ const formatCurrentWeather = (data) => {
     return { lat, lon, temp, feels_like, temp_min, temp_max, 
     humidity, name, dt, country, sunrise, sunset, details, icon, speed, }
 }
+// not getting daily and hourly in api change daily to main 
 
 const formatForcastWeather = (data) => {
-    let {timezone, daily, hourly} = data;
-    daily = daily.slice(1,6).map(d =>{
-        return {
-            title: formatToLocalTime(d.dt, timezone, 'ccc'),
-            temp: d.temp.day,
-            icon: d.weather[0].icon
-        }
+    let { timezone, main, hourly } = data;
+    main = (main?.slice(1, 6) ?? []).map((d) => {
+      return {
+        title: formatToLocalTime(d.dt, timezone, 'ccc'),
+        temp: d.main.temp,
+        icon: d.weather[0].icon,
+      };
     });
-
-    hourly = hourly.slice(1,6).map(d =>{
-        return {
-            title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
-            temp: d.temp.day,
-            icon: d.weather[0].icon
-        }
-});
-
-return { timezone,daily, hourly };
-
-};
+  
+    hourly = (hourly?.slice(1, 6) ?? []).map((d) => {
+      return {
+        title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
+        temp: d.temp.day,
+        icon: d.weather[0].icon,
+      };
+    });
+  
+    return { timezone, main, hourly };
+  };
+  
 
 const getFormattedWeatherData = async (searchParams) => {
     const formattedCurrentWeather = await getWeatherData(
@@ -60,17 +61,21 @@ const getFormattedWeatherData = async (searchParams) => {
         ).then(formatCurrentWeather)
         const {lat, lon} = formatCurrentWeather
 
-        const formattedForcastWeather = await getWeatherData('onecall', {
+        const formattedForcastWeather = await getWeatherData('weather', {
             lat, 
             lon, 
-            exclude: 'current,minutely, alerts', 
+            exclude: 'current, minutely, alerts', 
             units: searchParams.units,
-        }).then(formatForcastWeather);
+        }).then(formatForcastWeather); 
 
-    return {...formattedCurrentWeather, ...formattedForcastWeather};
+    return {...formattedCurrentWeather, ...formattedForcastWeather}; // , 
 };
 
 const formatToLocalTime = (secs, zone, format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a") =>
 DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
+const iconUrlFromCode = (code) => `http://openweathermap.orp/img/wn/${code}@2x.png`;
+
 export default getFormattedWeatherData
+
+export {formatToLocalTime, iconUrlFromCode}  ;
